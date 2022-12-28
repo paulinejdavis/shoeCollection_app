457a2b0080772e9dae8ad1f512d06ea0ee69d306
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-28 17:42:02 +0000

    add background image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import logo from './images/logo.png';
+import image from './images/background.jpg';
 import './App.css';
 
 
 
 function App() {
   return (
+  
     <div className="App">
+        <div className="image" style={{ backgroundImage:`url(${image})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
       <img className="logo" src={logo} alt="Logo" />
      
      <div className="container">
       <div className="top">
         <input type="text" placeholder="Add a shoe..." />
+       
+      </div>
       </div>
      </div>
     </div>
   );
 }
 
-export default App;
+export default App ;

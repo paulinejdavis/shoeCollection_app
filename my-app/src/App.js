c63e091e79631b1de2623d368db9ff8d8bb5e8e9
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-12-28 17:13:59 +0000

    add title image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
-//import logo from './logo.svg';
+import React from 'react';
+import logo from './images/logo.png';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
+      <img className="logo" src={logo} alt="Logo" />
      
+     <div className="container">
+      <div className="top">
+        <input type="text" placeholder="Add a shoe..." />
+      </div>
+     </div>
     </div>
   );
 }
